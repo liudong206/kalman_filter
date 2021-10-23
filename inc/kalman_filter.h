650e3ec082cbb2dbfbfed746a564228094211e59
liudong206 <1072169613@qq.com>
2021-10-23 14:03:28 +0800

    kalman_filter 一维滤波

---
+++ inc/kalman_filter.h
@@ -0,0 +1,24 @@
+#ifndef  _KALMAN_FILTER_H
+#define  _KALMAN_FILTER_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+typedef struct {
+    float x;  /* state */
+    float A;  /* x(n)=A*x(n-1)+u(n),u(n)~N(0,q) */
+    float C;  /* z(n)=H*x(n)+w(n),w(n)~N(0,r)   */
+    float q;  /* process(predict) noise convariance */
+    float r;  /* measure noise convariance */
+    float p;  /* estimated error convariance */
+    float gain;
+} kalman_state;
+
+extern void kalman_init(kalman_state *state, float init_x, float init_p);
+extern float kalman_filter(kalman_state *state, float z_measure);
+
+#ifdef __cplusplus
+}
+#endif
+#endif  /*_KALMAN_FILTER_H*/
+
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream> 
+#include "kalman_filter.h"
+using namespace std;
+int main()
+{
+    kalman_state state; 
+    kalman_init(&state,0.0,1000.0);
+    char c[10];
+    ifstream inFile("log/tarSteerWheelAngle.xls", ios::in | ios::binary);
+    ofstream outFile("log/tarSteerWheelAngle_filter.xls",std::ios::app);
+    if (!inFile) {
+        cout << "open file error" << endl;
+        return 0;
+    }
+    //连续以行为单位，读取 in.txt 文件中的数据
+    while (inFile.getline(c, 320)) {
+        float ret = strtol(c,NULL,10);
+        float out = kalman_filter(&state,ret);
+        if(!outFile.fail()){
+            outFile<<out<<endl;
+        }
+        else {
+            cout<<"open failed"<<endl;
+        }
+    }
+    inFile.close();
+    cout<<"kalman filter finished"<<endl;
+    return 1;
+}
